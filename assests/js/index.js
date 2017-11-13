18595dcdeb048fd1ec96a07ca466ed5c6fe61839
JC <JC>
2017-11-13 15:17:15 -0600

    completed

--- assests/js/index.js
+++ assests/js/index.js
@@ -1,3 +1,31 @@
 //// JavaScript source code
 console.log("Connected");
 
+///Check off Specific Todos by Clicking
+$("ul").on("click", "li", function() {
+    $(this).toggleClass("completed");
+});
+
+//Click on X to delete Todo
+$("ul").on("click", "span", function(event) {
+    alert("Clicked on a span");
+    $(this).parent().fadeOut(500,
+        function() {
+            $(this).remove();
+        });
+    event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(event) {
+    if (event.which === 13) {
+        //grabbing new text from input
+        var todoText = $(this).val();
+        $(this).val("");
+        //create new li and add to ul
+        $("ul").append("<li><span><i class='fa fa-trash'></i> </span>" + todoText + "</li>");
+    }
+});
+
+$(".fa-plus").click(function() {
+    $("input[type='text']").fadeToggle();
+})
