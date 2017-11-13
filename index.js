0728e1026dc5b70bad108f9710f582bf198d0290
JC <JC>
2017-11-13 12:55:17 -0600

    updated

--- index.js
+++ index.js
@@ -1,26 +1,8 @@
 //// JavaScript source code
 console.log("Connected");
 
-var lis = document.querySelectorAll("li");
-
-for (var i = 0; i < lis.length; i++) {
-    lis[i].addEventListener("mouseover",
-        function() {
-            this.classList.add("selected");
-        });
-    lis[i].addEventListener("mouseout",
-        function() {
-            this.classList.remove("selected");
-        });
-    lis[i].addEventListener("click",
-        function() {
-            this.classList.toggle("done");
-        });
-};
-
-var events = document.getSelection("a");
-
-for (var aCount = 0; aCount > -1; aCount++);
-{
-    console.log(aCount);
-};
+$('button').on('click',
+    function() {
+        $('div').fadeOut(1000);
+        console.log("this");
+    });
